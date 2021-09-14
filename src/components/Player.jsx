09590418d310f611d2c5707b2f7a266367b51f47
Christoffer Jörgensen <christoffer.joergensen@gmail.com>
2021-09-14 23:03:15 +0200

    Added ReactPlayer, control icons, play and pause methods.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactPlayer from 'react-player/youtube'
 
 const Player = () => {
+
+    let [playing, setPlaying] = useState(false);
+    
     return ( 
-        <h1>This is a player</h1>
+        <div>
+            <ReactPlayer url='http://www.youtube.com/watch?v=ysz5S6PUM-U' width="0px" height="0px" playing={playing} />
+            <i className="fas fa-step-backward"></i>
+            <i className="far fa-play-circle" onClick={ () => setPlaying(true)}></i>
+            <i className="far fa-pause-circle" onClick={ () => setPlaying(false)}></i>
+            <i className="fas fa-step-forward"></i>
+        </div>
      );
 }
  
