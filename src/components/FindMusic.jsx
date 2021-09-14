2ba5f04f8a3b9e41664066c6aa43a99c9982b218
Christoffer Jörgensen <christoffer.joergensen@gmail.com>
2021-09-14 23:07:29 +0200

    Added form inputs, async useEffect for fetching API data and useState for searchString and searchResults.

--- src/components/FindMusic.jsx
+++ src/components/FindMusic.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const FindMusic = () => {
-    return ( 
-        <h1>Search for music</h1>
-     );
+
+    const [searchString, setSearchString] = useState('Master of Puppets');
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(async () => {
+        let result = await fetch('https://yt-music-api.herokuapp.com/api/yt/search/' + searchString)
+        let data = await result.json()
+        console.log(data)
+    },[])
+
+    return (
+        <div>
+            <form>
+                <label>
+                    Find music:
+                    <input type="search" name="search" />
+                </label>
+                <input type="submit" value="Search" />
+            </form>
+        </div>
+    );
 }
- 
+
 export default FindMusic;
