3869f3c648d1f0ec2411f8d1a698a03cdd5b67fa
Christoffer Jörgensen <christoffer.joergensen@gmail.com>
2021-09-14 23:05:52 +0200

    Added styles, logo component and ReactPlayer dependencies.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import NavBar from './components/NavBar'
 import PlayList from './components/Playlist'
 import FindMusic from './components/FIndMusic'
 import Player from './components/Player'
+import Logo from './components/Logo'
 
 function App() {
 
   return (
     <div className="App">
       <div className="Navbar">
+        <Logo />
         <NavBar />
       </div>
       <div className="Content">
