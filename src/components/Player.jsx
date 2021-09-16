6d627e67770130aa60eed58977c3b78f6d8346d4
Christoffer Jörgensen <christoffer.joergensen@gmail.com>
2021-09-16 23:38:17 +0200

    Added nextSong() and previousSong() functions. Added useRef to useEffect to prevent render on startup. Added automatic playback of next song in queue. Started implementing song duration and playback progress.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,77 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import ReactPlayer from 'react-player/youtube'
 import { useContext, useEffect } from 'react';
 import { Context } from '../Store';
+import CurrentSong from './CurrentSong';
 
 const Player = () => {
     const [state, dispatch] = useContext(Context);
-    let [playing, setPlaying] = useState(false);
+    const [playing, setPlaying] = useState(false);
+    const [duration, setDuration] = useState(0);
+    const [progress, setProgress] = useState({playedSeconds: 0});
+    const [playedMinutes, setPlayedMinutes] = useState(0);
+    const [playedSeconds, setPlayedSeconds] = useState(0);
+    const notInitialRender = useRef(false)
 
-    useEffect( () =>{
-        setPlaying(true);
-    },[state.playing])
+    useEffect(() => {
+        if (notInitialRender.current) {
+            setPlaying(true);
+            setPlayedMinutes(Math.floor(progress.playedSeconds / 60))
+            setPlayedSeconds(duration - playedMinutes * 60)
+        } else {
+          notInitialRender.current = true
+        }
+      }, [state.playing])
+
+      useEffect(() => {
+        if (notInitialRender.current) {
+            setPlayedMinutes(Math.floor(progress.playedSeconds / 60))
+            setPlayedSeconds((progress.playedSeconds - playedMinutes * 60).toFixed())
+        } else {
+          notInitialRender.current = true
+        }
+      }, [progress.playedSeconds])
+
+      
+
+    const nextSong = () =>{
+        for(var i = 0; i < state.currentPlaylist.length - 1; i++){
+            if(state.currentPlaylist[i].videoId == state.playing.videoId){
+                dispatch({ type: 'SET_PLAYING', payload: state.currentPlaylist[i+1]});
+            }
+        }
+    }
+
+    const previousSong = () =>{
+        for(var i = 0; i < state.currentPlaylist.length - 1; i++){
+            if(state.currentPlaylist[i].videoId == state.playing.videoId && i != 0){
+                dispatch({ type: 'SET_PLAYING', payload: state.currentPlaylist[i-1]});
+            }
+        }
+    }
     
-    return ( 
-        <div>
-            <ReactPlayer url={'http://www.youtube.com/watch?v=' + state.playing.videoId} width="0px" height="0px" playing={playing} />
-            <i className="fas fa-step-backward"></i>
-            <i className="far fa-play-circle" onClick={ () => setPlaying(true)}></i>
-            <i className="far fa-pause-circle" onClick={ () => setPlaying(false)}></i>
-            <i className="fas fa-step-forward"></i>
-        </div>
-     );
+    return (
+        <>
+            <div className="CurrentSong">
+                <CurrentSong isPlaying={playing} />
+            </div>
+            <div className="Controls">
+                <ReactPlayer 
+                    url={'http://www.youtube.com/watch?v=' + state.playing.videoId} 
+                    width="0px" height="0px" 
+                    playing={playing}
+                    onDuration={setDuration}
+                    onProgress={setProgress} 
+                    onEnded={ () => nextSong() }/>
+                <i className="fas fa-step-backward" onClick={() => previousSong()}></i>
+                <i className="far fa-play-circle" onClick={() => setPlaying(true)}></i>
+                <i className="far fa-pause-circle" onClick={() => setPlaying(false)}></i>
+                <i className="fas fa-step-forward" onClick={() => nextSong()}></i>
+            </div>
+            <div><p>Duration: {((duration / 60)).toFixed(2)}<br/>
+            Played: {playedMinutes}:{playedSeconds}</p></div>
+        </>
+    );
 }
  
 export default Player;
