78fbba0b6713de6345976600edd63d929d3189c8
Christoffer Jörgensen <christoffer.joergensen@gmail.com>
2021-09-16 23:34:13 +0200

    Added CurrentSong.jsx component displaying info about song currently playing.

---
+++ src/components/CurrentSong.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState, useContext } from "react";
+import { Context } from "../Store";
+
+const CurrentSong = (props) => {
+    
+    const [state, dispatch] = useContext(Context);
+    const [thumbnail, setThumbnail] = useState('');
+    const [artist, setArtist] = useState('');
+    const [songName, setSongName] = useState('');
+
+    useEffect( () =>{
+        if(props.isPlaying){
+            setThumbnail(state.playing.thumbnails[0].url);
+            setArtist(state.playing.artist.name);
+            setSongName(state.playing.name);
+        }
+    }, [props.isPlaying, state.playing])
+
+    return (
+        <>
+            <div>
+                <img src={thumbnail}></img>
+            </div>
+            <div>
+                <h5>{songName}</h5>
+                <p>{artist}</p>
+            </div>
+        </>
+    );
+}
+
+export default CurrentSong;
