8a17d32394359964b586fedc2fed7db2841c343e
Christoffer Jörgensen <christoffer.joergensen@gmail.com>
2021-09-20 12:37:31 +0200

    Added layout for Playlist.jsx component.

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect, useContext, useRef } from 'react'
+import { Context } from '../Store';
+import Songs from './Songs';
 
 const PlayList = () => {
+    const [state, dispatch] = useContext(Context);
     return (
-        <div className="Playlist">
-            <h1>This is a playlist</h1>
-        </div> 
-     );
+        <div className="Playlists">
+            <div>
+                <h2>Current Playlist</h2>
+            </div>
+            <div className="FindMusic">
+                <div className="Results">
+                    <div className="Songs">
+                        {state.currentPlaylist.map(song =>
+                            <Songs key={song.videoId} song={song} />)}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
- 
+
 export default PlayList;
