6821616b7ad15448bf5c98983b62d380f8079352
Christoffer Jörgensen <Christoffer.Jorgensen@elicit.se>
2021-10-02 19:54:07 +0200

    Added Shuffle Playlist button. Added shufflePlaylist and getRandomInt functions. Modified CSS for mobile  portrait viewports.

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -29,6 +29,24 @@ const PlayList = () => {
         dispatch({ type: 'SET_CUSTOM_PLAYLIST', payload: [] })
     }
 
+    const getRandomInt = (min, max) =>{
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    const shufflePlaylist = () =>{
+        let copiedCustomPlaylist = [...state.customPlaylist]
+        let shuffledCopiedPlaylist = []
+
+        for(let i = 0; copiedCustomPlaylist.length > 0; i++){
+            let randNbr = getRandomInt(0, copiedCustomPlaylist.length)
+            shuffledCopiedPlaylist.push(copiedCustomPlaylist[randNbr])
+            copiedCustomPlaylist.splice(randNbr, 1)
+            dispatch({ type: 'SET_CUSTOM_PLAYLIST', payload: shuffledCopiedPlaylist })
+        }
+    }
+
     if (state.customPlaylist.length) {
         return (
             <div className="Playlists">
@@ -37,6 +55,7 @@ const PlayList = () => {
                     <button id="Save" onClick={ () => savePlaylist() }>Save Playlist</button>
                     <button id="Load" onClick={ () => loadPlaylist() }>Load Playlist</button>
                     <button id="Delete" onClick={ () => deletePlaylist() }>Delete Playlist</button>
+                    <button id="Shuffle" onClick={ () => shufflePlaylist() }>Shuffle Playlist</button>
                 </div>
                 <div className="FindMusic">
                     <div className="Results">
